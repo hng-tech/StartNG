dbb86fcf4836e16161136cac0ab0a2d588abc361
JeffOgah <jeff.ogah@gmail.com>
2019-08-13 23:14:03 +0100

    collect subscription mails

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php  session_start();    
 
-file_put_contents("data.json", json_encode($_POST)."\r\n",FILE_APPEND);
-file_put_contents("email.txt", $_POST['email']."\r\n",FILE_APPEND);
+file_put_contents("internship-email.json", json_encode($_POST)."\r\n",FILE_APPEND);
+file_put_contents("internship-email.txt", $_POST['email']."\r\n",FILE_APPEND);
 $_SESSION['submit'] = "done";
 header("Location: index.php");
 
