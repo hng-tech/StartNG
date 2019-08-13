4ec99604e836f2325273523691ff7a11cda4a670
JeffOgah <jeff.ogah@gmail.com>
2019-08-13 21:13:23 +0100

    removed unneeded JS scripts

--- index.php
+++ index.php
@@ -16,9 +16,6 @@
   <!-- CSS and JS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <link rel="stylesheet" type="text/css" href="styles/index.css" />
-  <script src="scripts/index.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </head>
@@ -39,7 +36,7 @@
       <div class="col-sm-12 col-lg-6  px-4 pt-2">
         <div class="text-center mt-sm-0 mt-md-5">
           <h5 class="font-weight-bold">Pre-Internship registration has closed</h5>
-          <h5 class="font-weight-bold my-3">Visit <a href="https://hng.tech/register">HNG.Tech</a> to register for the main internship</h5>
+          <h5 class="font-weight-bold my-3">Visit <a href="https://hng.tech">HNG.Tech</a> to register for the main internship</h5>
           <p>Follow us on twitter to get updated news <a href="https://twitter.com/hnginternship"><img src="https://www.itsnicethat.com/system/files/062012/4fd07cea5c3e3c0d810000db/images_slice_large/Twitternew.jpg" width="10%" /></a></p>
         </div>
       </div>
