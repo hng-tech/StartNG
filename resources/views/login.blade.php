a92ff80065d18cb51d68971149ac56f7ea0ff9fc
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-09 17:02:34 +0100

    cert fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,8 +20,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link href="http://allfont.net/allfont.css?fonts=blackchancery" rel="stylesheet" type="text/css" />
     <style>
-        body {
+        /* body {
             font-family: 'Lato';
             color: #084482;
         }
@@ -37,11 +38,54 @@
         }
         @media (max-width: 780px) {
             #shn-code {width: 50%;}
+            
+        } */
+        body {
+            font-family: 'Lato';
+            /* background-image: url('{{ asset('images/startng.png') }}'); */
         }
+        h1, h5 {color: #084482;font-weight:bold;}
+        h1 {margin-top: 45pt; font-size:48pt;}
+        h3 {margin-top: -10pt; font-size: 36pt;color: #FFA800;font-weight:bold;}
+        h5 {margin-top: 17pt; font-size: 28pt;}
+        p {font-size:16pt;}
+        .link {
+            float: right;
+            margin-top: 17pt;
+            font-size: 10pt;
+        }
+        .intern-name {
+            /* font-family: 'BlackChancery', 'Roboto'; */
+        }
+        .support {margin: 0 15pt 24pt 0;}
     </style>
 </head>
 <body>
-    <div class="container-fluid">
+<div class="container" style="">
+    <p class="link">HNG Internship has confirmed the participation of this individual in<br> this program. Confirm at <a href="http://">start.ng/verify/seyi-onifade-shn001</a></p>
+    <h1>CERTIFICATE</h1>
+    <h3>OF COMPLETION</h3>
+    <P style="margin-top: 31pt;">This is to certify that</P>
+    <p style="font-size:37pt;">Seyi Onifade Olutotr</p>
+    <p style="margin-bottom: -12pt;">has successfully completed</p>
+    <h5>Startdotng (the HNG Pre-Internship)</h5>
+    <p style="margin-bottom: 50pt;">with 80% task completion on <strong>21st September, 2019</strong></p>
+    <p style="font-size:24pt;margin-bottom: -5pt;">Seyi Onifade </p>
+    <p style="">CEO, HNG Internship</p>
+    <p style="margin-top: 28pt;">Supported by</p>
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+    <img src="{{ asset('images/startng.png') }}" alt="" class="support">
+</div>
+
+
+
+
+
+    <!-- <div class="container-fluid">
         <nav class="navbar navbar-light mt-sm-2">
             <a class="navbar-brand" href="#">
                 <img src="{{ asset('images/startng.png') }}" alt="" srcset="">
@@ -68,6 +112,6 @@
     </div>
     <footer class="container-fluid bg-primary">
         <p>&copy; 2019 HNG Internship. All rights reserved.</p>
-    </footer>
+    </footer> -->
 </body>
 </html>
