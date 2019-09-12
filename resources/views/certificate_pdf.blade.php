d9ceaa4eccbe623f644ecd93135b4cf4e0539153
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-12 19:40:50 +0100

    date fix

--- resources/views/certificate_pdf.blade.php
+++ resources/views/certificate_pdf.blade.php
@@ -74,7 +74,7 @@
     <p style="font-size:37pt;margin-bottom: 5pt;">{{strtoupper($data['name'])}}</p>
     <p style="margin-bottom: -12pt;">has successfully completed</p>
     <h5>Startdotng (Beginner Software Development Training)</h5>
-    <p style="margin-bottom: 30pt;">with {{$data['score']}} out of 100 obtainable points on <strong>21st September, 2019</strong></p>
+    <p style="margin-bottom: 30pt;">with {{$data['score']}} out of 100 obtainable points on <strong>9th September, 2019</strong></p>
     <p style="font-size:24pt;margin-bottom: -3pt; font-family: Pacifico, 'Oleo Script';"><i>Seyi Onifade </i></p>
     <p  style="font-size:12pt;margin-top: 0;">CEO, HNG Internship</p>
     <p style="margin-top: 28pt;">Supported by</p>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,7 +20,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
-    <link href="http://allfont.net/allfont.css?fonts=blackchancery" rel="stylesheet" type="text/css" />
+    <link href="http://allfont.net/allfont.css?fonts=blackchancery" rel="stylesheet" type="text/css">
     <style>
         body {
             font-family: 'Lato';
