53a8fdff68f2958e3465514c3be7ba45ba78264b
prismathic <pjjsalam1999@gmail.com>
2019-09-13 16:47:44 +0100

    Feat: Added verification index

---
+++ resources/views/verification.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verify Certificate</title>
+</head>
+<body>
+    Name : {{$name['name']}} <br>
+    Code : {{$name['code']}} <br>
+    Task 1 : {{$name['task1']}} <br>
+    Task 2 : {{$name['task2']}} <br>
+    Task 3 : {{$name['task3']}} <br>
+    Task 4 : {{$name['task4']}} <br>
+    Task 5 : {{$name['task5']}} <br>
+    Score : {{$name['score']}} <br>
+    Grade : {{$name['grade']}} <br>
+</body>
+</html>
