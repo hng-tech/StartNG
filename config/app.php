30ca5adb416e702e1f606fcc9077b0da1f714b23
prismathic <pjjsalam1999@gmail.com>
2019-09-16 14:17:40 +0100

    Added Fpdf Service Provider

--- config/app.php
+++ config/app.php
@@ -161,12 +161,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\Snappy\ServiceProvider::class,
-        Anouar\Fpdf\FpdfServiceProvider::class,
 
         /*
          * Package Service Providers...
          */
+        Anouar\Fpdf\FpdfServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -216,7 +215,6 @@ return [
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
-        'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
@@ -225,13 +223,12 @@ return [
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
-        'SnappyImage' => Barryvdh\Snappy\Facades\SnappyImage::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        
+
 
     ],
 
