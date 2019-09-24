5cab2248bb15f7a397ee02bffc158bb7d630bada
DmatrixNg <49094365+DmatrixNg@users.noreply.github.com>
2019-09-24 09:05:53 +0100

    fixed FTP security

--- public/articles/wp-config.php
+++ public/articles/wp-config.php
@@ -55,6 +55,7 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 define( 'UPLOADS', 'wp-content/uploads' );
+define('FS_METHOD','direct');
 set_time_limit(300);
 /**#@-*/
 
